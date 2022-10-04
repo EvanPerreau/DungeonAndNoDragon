3336c043531dff14127068341ba980d52d8f42c8
Valou0539 <87665858+Valou0539@users.noreply.github.com>
2022-10-04 11:10:55 +0200

    annotations

--- D&D/D&noD/ArmorClass.py
+++ D&D/D&noD/ArmorClass.py
@@ -1,5 +1,7 @@
 
 class Armor(object):
+    """Armure avec un nom et des points d'armure"""
+    
     def __int__(self, name='Default', armor=0):
         assert isinstance(name, str)
         self.name = name
