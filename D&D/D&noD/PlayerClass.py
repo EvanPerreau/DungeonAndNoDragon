26984299f63d21c2bccebe8962573b0670273a05
Valou0539 <87665858+Valou0539@users.noreply.github.com>
2022-10-04 11:26:48 +0200

    annotations
    Correction de la méthode setArmor()

--- D&D/D&noD/PlayerClass.py
+++ D&D/D&noD/PlayerClass.py
@@ -3,6 +3,7 @@ from InventoryClass import *
 
 
 class Player(object):
+    """créer le joueur"""
 
     ####################
     #       INIT       #
@@ -35,55 +36,66 @@ class Player(object):
     ####################
 
     def setName(self, name):
+        """change le nom du joueur"""
         assert isinstance(name, str)
         self.name = name
         return
 
     def addHeal(self, amount):
+        """ajoute de la vie au joueur"""
         assert isinstance(amount, float)
         self.health += amount
         return
 
     def subtractHeal(self, amount):
+        """enlève de la vie au joueur"""
         assert isinstance(amount, float)
         self.health -= amount
         return
 
     def setHeal(self, amount):
+        """défini les points de vie du joueur"""
         assert isinstance(amount, float)
         self.health = amount
         return
 
     def addAttackDamage(self, amount):
+        """Ajoute des points d'attaque au joueur"""
         assert isinstance(amount, float)
         self.attack_damage += amount
         return
 
     def subtractAttackDamage(self, amount):
+        """réduit les points d'attaque du joueur"""
         assert isinstance(amount, float)
         self.attack_damage -= amount
         return
 
     def setAttackDamage(self, amount):
+        """définit les points d'attaque du joueur"""
         assert isinstance(amount, float)
         self.attack_damage = amount
         return
 
     def addArmor(self, amount):
+        """ajoute des points d'armure au joueur"""
         assert isinstance(amount, float)
         self.armor += amount
         return
 
     def subtractArmor(self, amount):
+        """retire des points d'armure au joueur"""
         assert isinstance(amount, float)
         self.armor -= amount
         return
 
     def setArmor(self, amount):
+        """définit les points d'armure du joueur"""
         assert isinstance(amount, float)
-        self.armor -= amount
+        self.armor = amount
         return
 
     def setInventory(self, inventory):
+        """définit l'inventaire du joueur"""
         assert isinstance(inventory, object)
         self.inventory = inventory
